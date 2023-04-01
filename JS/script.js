09ab8997dc809793f4365a027f3259a67ddee872
Aleksandr Shcheglov <shcheglovmusician@gmail.com>
2023-04-01 20:44:51 +0400

    refact: remove for loop

--- JS/script.js
+++ JS/script.js
@@ -2,24 +2,33 @@
 
 const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal-window');
-const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
-const addHiddenClass = function () {
+const buttonCloseModal = document.querySelector('.close-modal-window');
+const buttonsBox = document.querySelector('.buttons-box');
+const modalWindowTitle = document.querySelector('.modal-window-title');
+
+const closeModal = function () {
   modalWindow.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-const showModalWindow = function () {
-  modalWindow.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+
+const openModal = function (event) {
+  if (event.target.classList.contains('show-modal-window')) {
+    const title = event.target.textContent;
+
+    modalWindowTitle.textContent = title;
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  }
 };
 
-for (let i = 0; i < btnsShowModalWindow.length; i++) {
-  btnsShowModalWindow[i].addEventListener('click', showModalWindow);
-}
-btnCloseModal.addEventListener('click', addHiddenClass);
-overlay.addEventListener('click', addHiddenClass);
+buttonCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+buttonsBox.addEventListener('click', function (event) {
+  openModal(event);
+});
 
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape' && !modalWindow.classList.contains('hidden'))
-    addHiddenClass();
+    closeModal();
 });
