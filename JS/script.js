43e7ac6d43c35b47c02510d9f55fefd9f05d24bc
NikFish <119619032+NikFish@users.noreply.github.com>
2023-04-01 12:40:45 +0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -4,16 +4,17 @@ const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal-window');
 const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
-const closeModalWindow = function () {
+const addHiddenClass = function () {
   modalWindow.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+const showModalWindow = function () {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsShowModalWindow.length; i++) {
-  btnsShowModalWindow[i].addEventListener('click', function () {
-    modalWindow.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-  btnCloseModal.addEventListener('click', closeModalWindow);
-  overlay.addEventListener('click', closeModalWindow);
+  btnsShowModalWindow[i].addEventListener('click', showModalWindow);
+  btnCloseModal.addEventListener('click', addHiddenClass);
+  overlay.addEventListener('click', addHiddenClass);
 }
